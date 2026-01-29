c89b27fe0d4d6528d55e764dbb1605d8f91f7728
Litudalei <gitiranjan815@gmail.com>
2026-01-29 16:56:56 +0530

    [gitiranjan] Added: UC2: Render oops to star pattern

--- src/OopsBanner.java
+++ src/OopsBanner.java
@@ -1,5 +1,39 @@
 public class OopsBanner {
+
     public static void main(String[] args) {
-        System.out.println("oops");
+        // Line 1
+        System.out.println(
+                "  *****  " + "  *****  " + " ******  " + "  *****  "
+        );
+
+        // Line 2
+        System.out.println(
+                " *     * " + " *     * " + " *     * " + " *        "
+        );
+
+        // Line 3
+        System.out.println(
+                " *     * " + " *     * " + " *     * " + " *        "
+        );
+
+        // Line 4
+        System.out.println(
+                " *     * " + " *     * " + " ******  " + "  *****  "
+        );
+
+        // Line 5
+        System.out.println(
+                " *     * " + " *     * " + " *        " + "       * "
+        );
+
+        // Line 6
+        System.out.println(
+                " *     * " + " *     * " + " *        " + "       * "
+        );
+
+        // Line 7
+        System.out.println(
+                "  *****  " + "  *****  " + " *        " + "  *****  "
+        );
     }
 }
